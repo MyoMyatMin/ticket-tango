fd6d7f48cb8bf80db81d33bec96139e5995d717b
MyoMyatMin <myomyatmin315@gmail.com>
2024-09-13 23:45:41 +0700

    Update quick select with date

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <AppBar position="static" color="secondary">
+    <AppBar position="static" color="primary">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Ticket Tango
--- components/QuickSearch.js
+++ components/QuickSearch.js
@@ -10,7 +10,6 @@ import {
   MenuItem,
   FormControl,
   InputLabel,
-  TextField,
   Box,
   Typography,
 } from "@mui/material";
@@ -18,17 +17,43 @@ import {
 export default function QuickSearch() {
   const [theater, setTheater] = useState("");
   const [movie, setMovie] = useState("");
-  const [date, setDate] = useState(null);
+  const [dateTime, setDateTime] = useState("");
+
+  const [availableTimes, setAvailableTimes] = useState([
+    { id: 1, time: "Sun, 15 Sep 2024 10:00 AM" },
+    { id: 2, time: "Sun, 15 Sep 2024 1:00 PM" },
+    { id: 3, time: "Sun, 15 Sep 2024 4:00 PM" },
+    { id: 4, time: "Sun, 15 Sep 2024 7:00 PM" },
+    { id: 5, time: "Mon, 16 Sep 2024 10:00 AM" },
+    { id: 6, time: "Mon, 16 Sep 2024 1:00 PM" },
+    { id: 7, time: "Tue, 17 Sep 2024 4:00 PM" },
+    { id: 8, time: "Tue, 17 Sep 2024 7:00 PM" },
+  ]);
 
   const handleReset = () => {
     setTheater("");
     setMovie("");
-    setDate(null);
+    setDateTime("");
   };
 
   const handleSubmit = () => {
-    console.log("Booking:", { theater, movie, date });
+    console.log("Booking:", { theater, movie, dateTime });
   };
+
+  const groupTimesByDate = () => {
+    const groupedTimes = availableTimes.reduce((acc, current) => {
+      const datePart = current.time.split(" ").slice(0, 4).join(" ");
+      if (!acc[datePart]) {
+        acc[datePart] = [];
+      }
+      acc[datePart].push(current);
+      return acc;
+    }, {});
+
+    return groupedTimes;
+  };
+
+  const groupedTimes = groupTimesByDate();
 
   return (
     <>
@@ -63,6 +88,36 @@ export default function QuickSearch() {
                 <MenuItem value="movie1">Movie 1</MenuItem>
                 <MenuItem value="movie2">Movie 2</MenuItem>
                 <MenuItem value="movie3">Movie 3</MenuItem>
+              </Select>
+            </FormControl>
+
+            <FormControl fullWidth>
+              <InputLabel id="dateTime-label">Date and Time</InputLabel>
+              <Select
+                native
+                labelId="dateTime-label"
+                value={dateTime}
+                label="Date & Time"
+                onChange={(e) => setDateTime(e.target.value)}
+                MenuProps={{
+                  PaperProps: {
+                    style: {
+                      maxHeight: 200,
+                      overflow: "auto",
+                    },
+                  },
+                }}
+              >
+                <option aria-label="None" value="" />
+                {Object.keys(groupedTimes).map((date) => (
+                  <optgroup key={date} label={date}>
+                    {groupedTimes[date].map((time) => (
+                      <option key={time.id} value={time.id}>
+                        {time.time}
+                      </option>
+                    ))}
+                  </optgroup>
+                ))}
               </Select>
             </FormControl>
           </Box>
