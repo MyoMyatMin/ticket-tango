9b6756c1a7be060857cabdf698240f5d3b101213
YeMyat144 <149359862+YeMyat144@users.noreply.github.com>
2024-09-15 13:01:47 +0700

    Added admin movie page to include form for adding movies

--- app/admin/movie/page.js
+++ app/admin/movie/page.js
@@ -1,9 +1,55 @@
+"use client";
 
-
+import { Box, Button, FormControl, Stack, TextField, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 
 const Movie = () => {
-    return <div>Admin/Movie</div>;
-  };
-  
+  return (
+    <Box sx={{ flexGrow: 1}}>
+      <Grid container spacing={2}>
+
+        <Grid size={6}>
+          <Stack spacing={2}>
+            <FormControl fullWidth>
+              <TextField
+                required
+                id="movie-title"
+                label="Title"
+              />
+            </FormControl>
+
+            <FormControl fullWidth>
+              <TextField
+                required
+                id="movie-genre"
+                label="Genre"
+              />
+            </FormControl>
+
+            <FormControl fullWidth>
+              <TextField
+                required
+                id="movie-duration"
+                label="Duration"
+              />
+            </FormControl>
+
+            <FormControl fullWidth>
+              <TextField
+                required
+                id="movie-casts"
+                label="Casts"
+              />
+            </FormControl>
+
+            <Button variant="contained" sx={{ backgroundColor: '#f05a5a' }}>
+              Add
+            </Button>
+          </Stack>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
 export default Movie;
-  
