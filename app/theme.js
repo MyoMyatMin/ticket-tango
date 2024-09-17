6eec54fa01a8bc0d33fecb01af55b6832ecfd44f
MyoMyatMin <myomyatmin315@gmail.com>
2024-09-17 20:55:12 +0700

    Add seat indicators and update theme.js

--- app/theme.js
+++ app/theme.js
@@ -1,52 +1,55 @@
-"use client";  
-import { createTheme } from "@mui/material/styles";  
+"use client";
+import { createTheme } from "@mui/material/styles";
 
-const theme = createTheme({  
-  palette: {  
-    primary: {  
-      main: "#FF6B6B", 
-    },  
-    secondary: {  
-      main: "#4ECDC4", 
-    },  
-    background: {  
-      default: "#1A1A2E",  
-      paper: "#1A1A2E", 
-    },  
-    text: {  
-      primary: "#FFD93D", 
-      secondary: "#e8e3e3", 
-    },  
-    error: {  
-      main: "#f44336", 
-    },  
-    warning: {  
-      main: "#ff9800", 
-    },  
-    info: {  
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FF6B6B",
+    },
+    secondary: {
+      main: "#4ECDC4",
+    },
+    background: {
+      default: "#1A1A2E",
+      paper: "#1A1A2E",
+    },
+    text: {
+      primary: "#FFD93D",
+      secondary: "#e8e3e3",
+    },
+    error: {
+      main: "#f44336",
+    },
+    warning: {
+      main: "#ff9800",
+    },
+    info: {
       main: "#2196f3",
-    },  
-    success: {  
-      main: "#4caf50",  
-    },  
+    },
+    success: {
+      main: "#4caf50",
+    },
+    disabled: {
+      main: "#000000",
+    },
   },
   components: {
     MuiOutlinedInput: {
       styleOverrides: {
         root: {
-          '& .MuiOutlinedInput-notchedOutline': {
-            borderColor: '#FF6B6B',
+          "& .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#FF6B6B",
           },
-          '&:hover .MuiOutlinedInput-notchedOutline': {
-            borderColor: '#4ECDC4',
+          "&:hover .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#4ECDC4",
           },
-          '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-            borderColor: 'red',
+          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            borderColor: "red",
           },
         },
       },
     },
   },
-});  
+});
 
 export default theme;
--- components/Theatre.js
+++ components/Theatre.js
@@ -3,430 +3,430 @@ import Seat from "@/components/Seat";
 import { useState } from "react";
 import { Typography, Stack, Box, Button } from "@mui/material";
 import Screen from "@/components/Screen";
-import Grid from '@mui/material/Grid2';
-
-const seats =  [
+import Grid from "@mui/material/Grid2";
+import ChairIcon from "@mui/icons-material/Chair";
+const seats = [
   {
-    "_id": "66e52846692f74b51e943f07",
-    "name": "Standard Seat 1",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f07",
+    name: "Standard Seat 1",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f08",
-    "name": "Standard Seat 2",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f08",
+    name: "Standard Seat 2",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f09",
-    "name": "Standard Seat 3",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f09",
+    name: "Standard Seat 3",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f0a",
-    "name": "Standard Seat 4",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f0a",
+    name: "Standard Seat 4",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f0b",
-    "name": "Standard Seat 5",
-    "type": "Standard",
-    "isAvailable": false,
-    "__v": 0
+    _id: "66e52846692f74b51e943f0b",
+    name: "Standard Seat 5",
+    type: "Standard",
+    isAvailable: false,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f0c",
-    "name": "Standard Seat 6",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f0c",
+    name: "Standard Seat 6",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f0d",
-    "name": "Standard Seat 7",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f0d",
+    name: "Standard Seat 7",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f0e",
-    "name": "Standard Seat 8",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f0e",
+    name: "Standard Seat 8",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f0f",
-    "name": "Standard Seat 9",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f0f",
+    name: "Standard Seat 9",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f10",
-    "name": "Standard Seat 10",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f10",
+    name: "Standard Seat 10",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f11",
-    "name": "Standard Seat 11",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f11",
+    name: "Standard Seat 11",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f12",
-    "name": "Standard Seat 12",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f12",
+    name: "Standard Seat 12",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f13",
-    "name": "Standard Seat 13",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f13",
+    name: "Standard Seat 13",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f14",
-    "name": "Standard Seat 14",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f14",
+    name: "Standard Seat 14",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f15",
-    "name": "Standard Seat 15",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f15",
+    name: "Standard Seat 15",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f16",
-    "name": "Standard Seat 16",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f16",
+    name: "Standard Seat 16",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f17",
-    "name": "Standard Seat 17",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f17",
+    name: "Standard Seat 17",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f18",
-    "name": "Standard Seat 18",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f18",
+    name: "Standard Seat 18",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f19",
-    "name": "Standard Seat 19",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f19",
+    name: "Standard Seat 19",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f1a",
-    "name": "Standard Seat 20",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f1a",
+    name: "Standard Seat 20",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f1b",
-    "name": "Standard Seat 21",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f1b",
+    name: "Standard Seat 21",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f1c",
-    "name": "Standard Seat 22",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f1c",
+    name: "Standard Seat 22",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f1d",
-    "name": "Standard Seat 23",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f1d",
+    name: "Standard Seat 23",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f1e",
-    "name": "Standard Seat 24",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f1e",
+    name: "Standard Seat 24",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f1f",
-    "name": "Standard Seat 25",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f1f",
+    name: "Standard Seat 25",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f20",
-    "name": "Standard Seat 26",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f20",
+    name: "Standard Seat 26",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f21",
-    "name": "Standard Seat 27",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f21",
+    name: "Standard Seat 27",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f22",
-    "name": "Standard Seat 28",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f22",
+    name: "Standard Seat 28",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f23",
-    "name": "Standard Seat 29",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f23",
+    name: "Standard Seat 29",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f24",
-    "name": "Standard Seat 30",
-    "type": "Standard",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f24",
+    name: "Standard Seat 30",
+    type: "Standard",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f25",
-    "name": "Premium Seat 1",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f25",
+    name: "Premium Seat 1",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f26",
-    "name": "Premium Seat 2",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f26",
+    name: "Premium Seat 2",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f27",
-    "name": "Premium Seat 3",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f27",
+    name: "Premium Seat 3",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f28",
-    "name": "Premium Seat 4",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f28",
+    name: "Premium Seat 4",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f29",
-    "name": "Premium Seat 5",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f29",
+    name: "Premium Seat 5",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f2a",
-    "name": "Premium Seat 6",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f2a",
+    name: "Premium Seat 6",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f2b",
-    "name": "Premium Seat 7",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f2b",
+    name: "Premium Seat 7",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f2c",
-    "name": "Premium Seat 8",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f2c",
+    name: "Premium Seat 8",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f2d",
-    "name": "Premium Seat 9",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f2d",
+    name: "Premium Seat 9",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f2e",
-    "name": "Premium Seat 10",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f2e",
+    name: "Premium Seat 10",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f2f",
-    "name": "Premium Seat 11",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f2f",
+    name: "Premium Seat 11",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f30",
-    "name": "Premium Seat 12",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f30",
+    name: "Premium Seat 12",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f31",
-    "name": "Premium Seat 13",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f31",
+    name: "Premium Seat 13",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f32",
-    "name": "Premium Seat 14",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f32",
+    name: "Premium Seat 14",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f33",
-    "name": "Premium Seat 15",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f33",
+    name: "Premium Seat 15",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f34",
-    "name": "Premium Seat 16",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f34",
+    name: "Premium Seat 16",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f35",
-    "name": "Premium Seat 17",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f35",
+    name: "Premium Seat 17",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f36",
-    "name": "Premium Seat 18",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f36",
+    name: "Premium Seat 18",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f37",
-    "name": "Premium Seat 19",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f37",
+    name: "Premium Seat 19",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f38",
-    "name": "Premium Seat 20",
-    "type": "Premium",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f38",
+    name: "Premium Seat 20",
+    type: "Premium",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f39",
-    "name": "VIP Seat 1",
-    "type": "VIP",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f39",
+    name: "VIP Seat 1",
+    type: "VIP",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f3a",
-    "name": "VIP Seat 2",
-    "type": "VIP",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f3a",
+    name: "VIP Seat 2",
+    type: "VIP",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f3b",
-    "name": "VIP Seat 3",
-    "type": "VIP",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f3b",
+    name: "VIP Seat 3",
+    type: "VIP",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f3c",
-    "name": "VIP Seat 4",
-    "type": "VIP",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f3c",
+    name: "VIP Seat 4",
+    type: "VIP",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f3d",
-    "name": "VIP Seat 5",
-    "type": "VIP",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f3d",
+    name: "VIP Seat 5",
+    type: "VIP",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f3e",
-    "name": "VIP Seat 6",
-    "type": "VIP",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f3e",
+    name: "VIP Seat 6",
+    type: "VIP",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f3f",
-    "name": "VIP Seat 7",
-    "type": "VIP",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f3f",
+    name: "VIP Seat 7",
+    type: "VIP",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f40",
-    "name": "VIP Seat 8",
-    "type": "VIP",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f40",
+    name: "VIP Seat 8",
+    type: "VIP",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f41",
-    "name": "VIP Seat 9",
-    "type": "VIP",
-    "isAvailable": true,
-    "__v": 0
+    _id: "66e52846692f74b51e943f41",
+    name: "VIP Seat 9",
+    type: "VIP",
+    isAvailable: true,
+    __v: 0,
   },
   {
-    "_id": "66e52846692f74b51e943f42",
-    "name": "VIP Seat 10",
-    "type": "VIP",
-    "isAvailable": true,
-    "__v": 0
-  }
-]
+    _id: "66e52846692f74b51e943f42",
+    name: "VIP Seat 10",
+    type: "VIP",
+    isAvailable: true,
+    __v: 0,
+  },
+];
 
 const SeatSelection = () => {
   const [selectedSeats, setSelectedSeats] = useState([]);
@@ -440,16 +440,18 @@ const SeatSelection = () => {
   };
 
   return (
-    <Box sx={{
-      maxWidth: '600px'
-    }}>
+    <Box
+      sx={{
+        maxWidth: "600px",
+      }}
+    >
       <Screen />
 
       <Grid container spacing={4} justifyContent="center">
         <Grid item>
           <Grid container display="flex" justifyContent="center" spacing={2}>
             {seats
-              .filter(seat => seat.type === 'Standard')
+              .filter((seat) => seat.type === "Standard")
               .map((seat, index) => (
                 <Grid item xs={6} key={index}>
                   <Seat
@@ -467,7 +469,7 @@ const SeatSelection = () => {
         <Grid item>
           <Grid container display="flex" justifyContent="center" spacing={2}>
             {seats
-              .filter(seat => seat.type === 'Premium')
+              .filter((seat) => seat.type === "Premium")
               .map((seat, index) => (
                 <Grid item xs={2} key={index}>
                   <Seat
@@ -485,7 +487,7 @@ const SeatSelection = () => {
         <Grid item>
           <Grid container display="flex" justifyContent="center" spacing={2}>
             {seats
-              .filter(seat => seat.type === 'VIP')
+              .filter((seat) => seat.type === "VIP")
               .map((seat, index) => (
                 <Grid item xs={2} key={index}>
                   <Seat
@@ -499,9 +501,30 @@ const SeatSelection = () => {
               ))}
           </Grid>
         </Grid>
-        
       </Grid>
 
+      <Box mt={4} display="flex" justifyContent="center">
+        <Stack direction="row" spacing={4} alignItems="center">
+          <Stack direction="row" spacing={1} alignItems="center">
+            <ChairIcon sx={{ color: "primary.main" }} />
+            <Typography>Standard</Typography>
+          </Stack>
+
+          <Stack direction="row" spacing={1} alignItems="center">
+            <ChairIcon sx={{ color: "secondary.main" }} />
+            <Typography>Premium</Typography>
+          </Stack>
+
+          <Stack direction="row" spacing={1} alignItems="center">
+            <ChairIcon sx={{ color: "info.main" }} />
+            <Typography>VIP</Typography>
+          </Stack>
+          <Stack direction="row" spacing={1} alignItems="center">
+            <ChairIcon sx={{ color: "disabled.main" }} />
+            <Typography>Unavailable</Typography>
+          </Stack>
+        </Stack>
+      </Box>
       {/* <Typography align="center" sx={{ marginTop: 2 }}>
         Selected Seats: {selectedSeats.join(", ")}
       </Typography> */}
