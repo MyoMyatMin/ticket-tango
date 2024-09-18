51be1622f3e449be601b1add79878d364a280e21
YeMyat144 <149359862+YeMyat144@users.noreply.github.com>
2024-09-18 13:39:09 +0700

    Updated TicketPage component to include MoviePoster component and update styling

--- app/ticket/page.js
+++ app/ticket/page.js
@@ -1,4 +1,5 @@
 "use client"; 
+import MoviePoster from "@/components/MoviePoster";
 import React from "react";
 import { Divider, Typography, Box, Button, TextField, Stack } from "@mui/material";
 import Grid from '@mui/material/Grid2';
@@ -7,34 +8,34 @@ const TicketPage = () => {
   return (
       <Grid container spacing={4}>
         <Grid sx={{ paddingLeft:5, paddingRight: 4 }} size={4}>
-          <Box
-            component="img"
-            src="https://via.placeholder.com/150" 
-            alt="Movie Poster"
-            sx={{
-              width: "100%",
-              borderRadius: "10px",
-              boxShadow: "0 4px 8px rgba(0,0,0,0.2)",
-            }}
-          />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            ml: 4,
+            mb: 4,
+          }}
+        >
+          <MoviePoster imageUrl="https://image.tmdb.org/t/p/w500/6MKr3KgOLmzOP6MSuZERO41Lpkt.jpg" />
+        </Box>
         </Grid>
 
         <Grid sx={{ paddingLeft: 2 }} size={5}>
           <br />
           <Stack spacing={4}>
-            <Typography>
+            <Typography variant="h6">
               Title: Sample Movie
             </Typography>
-            <Typography>
+            <Typography variant="h6">
               Theatre Name: Sample Theatre
             </Typography>
-            <Typography>
+            <Typography variant="h6">
               Selected Seat: A1, A2
             </Typography>
-            <Typography>
+            <Typography variant="h6">
               Date Time: 12th Sept, 7:00 PM
             </Typography>
-            <Typography>
+            <Typography variant="h6">
               Total: $25
             </Typography>
             <Divider sx={{ borderColor: "secondary.main", marginX: 2, marginY: 2 }} />
