499b258178c59fc5784b8542a43c03eaf270f482
YeMyat144 <149359862+YeMyat144@users.noreply.github.com>
2024-09-20 18:54:32 +0700

    Add logo in header

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,8 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Logo from "@/public/ticket-tango.svg";
+import Image from "next/image";
 import Link from "next/link";
 import { Box } from "@mui/material";
 
@@ -10,7 +12,7 @@ const Header = () => {
     <AppBar position="sticky" color="primary" elevation={3}>
       <Toolbar sx={{ paddingX: 3 }}>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Ticket Tango
+          <Image src={Logo} alt="Ticket Tango" width={50} height={50} />
         </Typography>
         <Box sx={{ display: "flex", gap: 2 }}>
           <Button color="inherit" component={Link} href="/">
