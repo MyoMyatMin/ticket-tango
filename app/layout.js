fa79a99a98f0e3a441df63e409a0e676d821ef8d
YeMyat144 <149359862+YeMyat144@users.noreply.github.com>
2024-09-26 00:50:38 +0700

    Added favicon

--- app/layout.js
+++ app/layout.js
@@ -2,10 +2,23 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import Header from "../components/Header";
 import { CssBaseline, ThemeProvider, Box } from "@mui/material";
 import theme from "./theme";
+import { Shortcut } from "@mui/icons-material";
 
 export const metadata = {
   title: "My Next.js App",
   description: "This is a Next.js app",
+  icons:{
+    icon:[
+      '/favicon.co',
+    ],
+    apple:[
+       '/apple-touch-icon.png',
+    ],
+    shortcut:[
+      '/apple-touch-icon.png',
+    ]
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({ children }) {
